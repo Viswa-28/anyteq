a8fab41a9d3c0d8c0c5a5c166c9fab1c262feb83
Viswa-28 <viswadiago28@gmail.com>
2025-07-30 09:02:37 +0530

    banner

---
+++ js/script.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+  el: "#wave",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x410756,
+  shininess: 130.00,
+  waveHeight: 40.00,
+  waveSpeed: 1.40,
+  zoom: 0.65
+})
